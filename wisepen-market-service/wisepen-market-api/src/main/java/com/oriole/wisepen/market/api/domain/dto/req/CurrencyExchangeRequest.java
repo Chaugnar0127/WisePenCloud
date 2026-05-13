70f2bbdb05aab3baa3af74e74ec549e45d7f1517
Shengtao Chang <cst18602500098@163.com>
2026-05-13 23:32:08 +0800

    refactor(market): 添加SuperBuilder注解

--- wisepen-market-service/wisepen-market-api/src/main/java/com/oriole/wisepen/market/api/domain/dto/req/CurrencyExchangeRequest.java
+++ wisepen-market-service/wisepen-market-api/src/main/java/com/oriole/wisepen/market/api/domain/dto/req/CurrencyExchangeRequest.java
@@ -6,8 +6,10 @@ import com.oriole.wisepen.market.api.enums.ExchangeDirection;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
+@SuperBuilder
 public class CurrencyExchangeRequest extends InfoPointBase {
     @NotNull(message = "换汇方向不能为空")
     private ExchangeDirection exchangeDirection;
